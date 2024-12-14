96d40a827423d29b82f2b2f46749550407a44249
Leo Jiang <114763373+leojiangxd@users.noreply.github.com>
2024-12-13 23:34:51 -0500

    Initial commit

---
+++ src/main/java/com/triangled/overlaymod/OverlayModClient.java
@@ -0,0 +1,10 @@
+package com.triangled.overlaymod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class OverlayModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+        
+    }
+}
